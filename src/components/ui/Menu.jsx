ae9af6135855b741bec61abee853d9d24864412f
Oğuzhan Altun <altunnoguzhan@gmail.com>
2023-09-14 15:47:39 +0300

    Update Menu.jsx

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -5,7 +5,7 @@ function Menu({ title, items }) {
     <section>
       <h6 className="text-lg text-primary-brand-color">{title}</h6>
       <nav className="grid gap-y-7">
-        <ul className="flex flex-col gap-y-1 md:gap-y-1 md:gap-y-4 mt-4">
+        <ul className="flex flex-col gap-y-1 md:gap-y-4 mt-4">
           {items.map((item, key) => (
             <li key={key}>
               <a className="text-sm hover:text-brand-color" href="#">
